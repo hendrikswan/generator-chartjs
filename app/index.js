1cc979319333032b386d54b2d0474e632dc81d57
hendrik swanepoel <hendrik.swanepoel@gmail.com>
2014-07-07 11:19:56 +0200

    first draft

---
+++ app/index.js
@@ -0,0 +1,44 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+
+
+var ChartjsGenerator = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = require('../package.json');
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.installDependencies();
+      }
+    });
+  },
+
+  askFor: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay('Welcome to the simple Chartjs project generator!'));
+
+    done();
+  },
+
+  app: function () {
+
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+    this.copy('_gulpfile.js', 'gulpfile.js');
+    this.copy('_index.html', 'index.html');
+    this.copy('_main.css', 'stylesheets/main.css');
+  },
+
+  projectfiles: function () {
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+  }
+});
+
+module.exports = ChartjsGenerator;
---
+++ generator-chartjs/app/index.js
@@ -0,0 +1,45 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+var chalk = require('chalk');
+
+
+var ChartjsGenerator = yeoman.generators.Base.extend({
+  init: function () {
+    this.pkg = require('../package.json');
+
+    this.on('end', function () {
+      if (!this.options['skip-install']) {
+        this.installDependencies();
+      }
+    });
+  },
+
+  askFor: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay('Welcome to the simple Chartjs project generator!'));
+
+    done();
+  },
+
+  app: function () {
+    this.mkdir('app');
+    this.mkdir('app/templates');
+
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+
+    console.log('hello there');
+  },
+
+  projectfiles: function () {
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+  }
+});
+
+module.exports = ChartjsGenerator;
